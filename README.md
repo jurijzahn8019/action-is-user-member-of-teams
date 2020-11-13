6571bf2ea472ef594e5bb707164a676f8d19f28f
Jurij Zahn <jurij.zahn@otto.de>
2020-11-13 08:42:40 +0100

    feat(action): implement initial action

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # action-is-user-member-of-teams
+
 Outputs boolean indicating whether user is a member of one or more github teams
+
+This action will take the username and a list of team names in format `<org>/<team>`
+And then perform an api Query to fetch the team metadata and evaluate whether the user
+is a member of any of them.
+
+Then it will set a boolean result as output
+
+The Action is used in my org in some Project board automation workflows
+
+## Inputs
+
+### `token`
+
+**Required** Github Api Token `"World"`.
+
+### `username`
+
+**Required** Username of the github user to check: `anyuser3445`.
+
+### `teams`
+
+**Required** Comma-sparated list of teams to check membership: `org/team1,org/team2`.
+
+### `varName`
+
+If given, then action will export the result as this env Var `USER_IS_MEMBER`.
+
+## Outputs
+
+### `ismember`
+
+true if user is member of one or more teams
+
+## Example usage
+
+```yaml
+name: User was Assigned to Issue
+on:
+  issues: [assigned]
+
+jobs:
+  do-something:
+    steps:
+      - uses: jurijzahn8019/action-is-user-member-of-teams@v0.0.1
+        id: checker
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          username: ${{ github.event.assignee.login }}
+          teams: my-org/team1, myorg2/team-b
+          varName: USER_IS_MEMBER
+
+      - name: Do Something if user is one of my teams
+        if: ${{ steps.checker.outputs.ismember }}
+        run: |
+          do something with the issue
+```
+
+## Test run
+
+```bash
+DEBUG=* \
+  INPUT_TOKEN=FOOOOO \
+  INPUT_USERNAME=foobar \
+  INPUT_TEAMS=foo/bar \
+  node dist/index.cjs.js
+```
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+/* eslint-disable camelcase */
+import {
+  getInput,
+  setFailed,
+  setOutput,
+  exportVariable,
+  info,
+} from "@actions/core";
+import { getOctokit } from "@actions/github";
+import debug from "debug";
+
+const dbg = debug("action-dependabot-labels:index");
+
+export async function run(): Promise<void> {
+  dbg("Check whether user is memebr of teams");
+  try {
+    dbg("Retrieve inputs");
+    const token = getInput("token", { required: true });
+    const username = getInput("username", { required: true });
+    const varName =
+      getInput("varName", { required: false }) || "USER_IS_MEMBER";
+    const teams = getInput("teams", { required: true })
+      .split(",")
+      .map((t) => {
+        const [org, team] = t.trim().split("/");
+        return { org, team };
+      });
+    dbg("Check user: %s in teams: %o", teams);
+
+    dbg("Fetch teams by name and org");
+    const byOrg = [] as { name: string; teams: string[] }[];
+    teams.forEach((t) => {
+      let org = byOrg.find((o) => o.name === t.org);
+      if (!org) {
+        org = { name: t.org, teams: [] };
+        byOrg.push(org);
+      }
+
+      if (!org.teams.find((n) => n === t.team)) {
+        org.teams.push(t.team);
+      }
+    });
+
+    const oqs = byOrg.map(({ name, teams: orgTeams }) => {
+      const orgKey = name.replace(/\W/g, "_");
+
+      const tqs = orgTeams.map((t) => {
+        const teamkey = t.replace(/\W/g, "_");
+        return `${teamkey}: teams(first: 100, query: "${t}") { ...Teams }`;
+      });
+
+      return `${orgKey}: organization(login: "${name}") { ${tqs.join("\n")} }`;
+    });
+
+    const query = `fragment Teams on TeamConnection {
+      nodes { name members { nodes { name: login } } }
+    }
+    query { ${oqs.join("\n")} }`;
+
+    const client = getOctokit(token);
+    const data = await client.graphql<
+      Record<
+        string,
+        Record<
+          string,
+          {
+            nodes: { name: string; members: { nodes: { name: string }[] } }[];
+          }
+        >
+      >
+    >(query);
+
+    dbg("Process result data");
+    const users = Object.values(data)
+      .map((org) =>
+        Object.values(org).map((orgTeams) =>
+          orgTeams.nodes.map((t) =>
+            t.members.nodes.map((user) => ({ team: t.name, user: user.name }))
+          )
+        )
+      )
+      .flat(4);
+
+    dbg("Apply team filter to the users list");
+    const filtered = users.filter(({ team }) =>
+      teams.some((t) => team === t.team)
+    );
+
+    const res = filtered.find((u) => username === u.user);
+    if (res) {
+      dbg("User %s is member of given team: %s", res.user, res.team);
+      info(`User ${res.user} is member of team ${res.team}`);
+    } else {
+      dbg("User %s is not member of any of given teams", username);
+    }
+
+    setOutput("ismember", !!res);
+    exportVariable(varName, !!res);
+  } catch (e) {
+    dbg("Failed:", e);
+    setFailed(e.message);
+  }
+}
+
+export default run();
